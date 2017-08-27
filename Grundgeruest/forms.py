1eea79dbecb5c3b8aa80bd344f7a5b85a5e3749c
Ilja <ilja.goethel@web.de>
2017-08-27 13:55:44 +0200

    userena urls ins projekt verschoben, urls auf deutsch angepasst und eigenes SignupForm übergeben

--- Grundgeruest/forms.py
+++ Grundgeruest/forms.py
@@ -3,25 +3,36 @@ from hashlib import sha1
 import random
 from userena.forms import SignupForm
 
-class Anmeldeformular(SignupForm):
+def erzeuge_zufall(laenge, sonderzeichen=3):
+    """ gibt Zufallsstring zurück, wobei die Zeichen abwechselnd aus den 
+    Listen in s gewählt werden; sonderzeichen<4 wählt nicht alle Listen """
+    s = ['abcdefghijkmnopqrstuvwxyz',
+        'ABCDEFGHJKLMNPQRSTUVWXYZ',
+        '23456789_-', 
+        '@.%&+!$?/()#*']
+    zufall = []
+    for i in range(laenge):
+        zufall.append(random.sample(s[i % sonderzeichen], 1)[0])
+    return ''.join(zufall)    
+
+class FormularRegistrierung(SignupForm):
     """
     Kopiert und angepasst aus SigninFormOnlyEmail aus userena.forms
     
-    Habe die Länge des autogenerierten Namens verlängert. Man könnte anderes 
-    Zeug
+    Habe autogenerierten Namen geändert.
     """
     def __init__(self, *args, **kwargs):
-        super(Anmeldeformular, self).__init__(*args, **kwargs)
+        super(FormularRegistrierung, self).__init__(*args, **kwargs)
         del self.fields['username']
 
     def save(self):
         """ Generate a random username before falling back to parent signup form """
         while True:
-            username = sha1(str(random.random()).encode('utf-8')).hexdigest()[:20]
+            username = erzeuge_zufall(7)
             try:
                 get_user_model().objects.get(username__iexact=username)
             except get_user_model().DoesNotExist: break
 
         self.cleaned_data['username'] = username
-        return super(Anmeldeformular, self).save()
+        return super(FormularRegistrierung, self).save()
 
--- seite/urls.py
+++ seite/urls.py
@@ -17,17 +17,17 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-
 from django.views.generic import TemplateView#, ListView, DetailView, 
 
+from Grundgeruest import userena_urls
 
 urlpatterns = [
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^nutzer/', include('userena.urls', namespace='')),
     url(r'^olymp/', include('Wettbewerbe.urls')),
     url(r'^ehemalige/', include('Ehemalige.urls')),
     url(r'^todo/', include('Notizen.urls')),
+    url(r'^nutzer/', include(userena_urls, namespace='')),
 
     url(r'^impressum/$', 
         TemplateView.as_view(
